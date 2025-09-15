d6c01e30d98055cbfadd8dad991068d3f849addd
Aashish Shrestha <94103028+AashishShrestha007@users.noreply.github.com>
2025-09-15 21:50:57 +0545

    Add files via upload

---
+++ open-website.bat
@@ -0,0 +1,9 @@
+@echo off
+echo 🌹 Opening your beautiful proposal website...
+echo 💕 Get ready to make the magic happen!
+echo.
+start "" "index.html"
+echo.
+echo ✨ The website should open in your default browser!
+echo 💍 Best of luck with your proposal!
+pause
